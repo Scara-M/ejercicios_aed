dd26204ab2e0fd3a41ff29df4d57f44b2fe96a24
Marco Scarafia <marcoaugscarafia@gmail.com>
2025-02-01 18:57:19 -0300

    Estructuras secuenciales

--- ficha2/ficha2ej1.py
+++ ficha2/ficha2ej1.py
@@ -4,6 +4,7 @@ Leer dos números y calcular:
 La suma de sus cuadrados.
 El promedio de sus cubos.
 """
+
 # datos de entrada
 a = int(input('Ingresar primer número: '))
 b = int(input('Ingresar segundo número: '))
