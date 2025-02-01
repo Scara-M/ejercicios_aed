84234afdf036d1f6b37330d73d991cef1a9a8354
Marco Scarafia <marcoaugscarafia@gmail.com>
2025-02-01 18:49:21 -0300

    Estructuras condicionales

--- ficha4/ejercicio11.py
+++ ficha4/ejercicio11.py
@@ -7,3 +7,63 @@ y terminó en el año 2000).
 2. Determinar si alguna de las obras fue creada en un año que se ingresa por teclado.
 3. Informar la diferencia en años entre la obra más nueva y la más antigua.
 """
+# inicializar varibales
+ant_xx = False
+obra_ingresada = False
+
+# ingreso de años de los cuadros
+cuadro1 = int(input('Ingresar el año del primer cuadro: '))
+cuadro2 = int(input('Ingresar el año del segundo cuadro: '))
+cuadro3 = int(input('Ingresar el año del tercer cuadro: '))
+ingresado = int(input('Ingrese año: '))
+
+# verificar si los cuadros son anteriores a 1901
+if cuadro1 < 1901 and cuadro2 < 1901 and cuadro3 < 1901:
+    ant_xx = True
+
+# si alguna de las obras ingresadas es la que se crea por teclado
+if cuadro1 == ingresado or cuadro2 == ingresado or cuadro3 == ingresado:
+    obra_ingresada = True
+
+# determinar la obra nueva y la vieja
+if cuadro1 < cuadro2 and cuadro1 < cuadro3:
+    menor = cuadro1
+    if cuadro2 < cuadro3:
+        mayor = cuadro3
+    else:
+        mayor = cuadro2
+else:
+    if cuadro2 < cuadro3:
+        menor = cuadro2
+        if cuadro1 > cuadro3:
+            mayor = cuadro1
+        else:
+            mayor = cuadro3
+    else:
+        menor = cuadro3
+        if cuadro1 > cuadro3:
+            mayor = cuadro1
+        else:
+            mayor = cuadro3
+
+# diferencia entre la obra nueva y antigua
+diferencia = mayor - menor
+
+# salida por pantalla
+print('-'*45)
+# verifica si los cuadros ingresados son anteriores al siglo xx
+if ant_xx == True:
+    print('Todos los cuadros ingresados son anteriores al siglo XX')
+else:
+    print('No todos los cuadros son anteriores al siglo XX')
+
+# si alguna de las obras ingresadas es la que se crea por teclado
+if obra_ingresada == True:
+    print('Hay al menos una obra del catalogo que es la que se ingresó por catálogo')
+else:
+    print('No hay ninguna obra con el año ingresado')
+
+# -- 3 diferencia entre la obra más nueva y mas antigua
+print('El mayor es: ', mayor)
+print('El menor es: ', menor)
+print('La diferencia entre el mayor y el menor es de: ', diferencia)
--- ficha4/ejercicio8.py
+++ ficha4/ejercicio8.py
@@ -14,7 +14,7 @@ suma = dado1 + dado2
 print('Dado 1: ', dado1)
 print('Dado 2: ', dado2)
 print('La suma de los dados es ', suma)
-if dado1 == dado2 or suma % 2 != 0:
+if dado1 == dado2 or (suma % 2) != 0:
     print('GANÓ EL USUARIO')
 else:
     print('PERDIÓ EL USUARIO. Ganó la máquina')
